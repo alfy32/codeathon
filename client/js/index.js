f0317db5411a381d921ceacf0b2672c6386a315a
Alan Christensen <alfy32@gmail.com>
2014-04-26 12:28:40 -0600

    added some

--- client/js/index.js
+++ client/js/index.js
@@ -1,7 +1,24 @@
 $(document).ready(function () {
 
-  $.get('/event', function(data) {
-    console.log(data);
+  $.get('/event?cityid=1', function(resp) {
+    $('.events').empty();
+    $('.events').append('<h3>Upcomming Events</h3>');
+
+    for(var i in resp.data) {
+      var ev = resp.data[i];
+      console.log(ev);
+
+      var div = $('<div>');
+
+      div.attr('class', 'event');
+      div.append($('<p>').text(ev.eventid));
+      div.append($('<p>').text(ev.date));
+      div.append($('<p>').text(ev.description));
+
+      $('.events').append(div);
+    }
+
+    $('.events').append($('<div>').css('clear', 'both'));
   });
 
 });
