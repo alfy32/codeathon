5113bca4a1087c196e2b91ff0038431e82e19048
Alan Christensen <alfy32@gmail.com>
2014-04-26 12:54:44 -0600

    more style

--- client/js/index.js
+++ client/js/index.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
   $.get('/event?cityid=1', function(resp) {
     $('.events').empty();
     $('.events').append('<h3>Upcomming Events</h3>');
+    $('.events').append('<button onclick="addEvent()">Add Event</button>');
 
     for(var i in resp.data) {
       var ev = resp.data[i];
@@ -11,7 +12,7 @@ $(document).ready(function () {
       var div = $('<div>');
 
       div.attr('class', 'event');
-      div.append($('<p>').attr('class', 'date').text(ev.date));
+      div.append($('<p>').attr('class', 'date').text('Date: ' + ev.date + ' Time: ' + ev.time));
       div.append($('<p>').attr('class', 'desc').text(ev.description));
 
       $('.events').append(div);
@@ -21,3 +22,7 @@ $(document).ready(function () {
   });
 
 });
+
+function addEvent() {
+  window.location = 'AddEvent.html';
+}
